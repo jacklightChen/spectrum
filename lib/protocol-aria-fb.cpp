83952038874936e3cbcd1fb334114f12ea646f62
Y-jiji <y.jijiji.data.science@gmail.com>
2024-03-05 11:01:45 +0800

    Feature: aria transaction execute.

--- lib/protocol-aria-fb.cpp
+++ lib/protocol-aria-fb.cpp
@@ -3,4 +3,62 @@
 namespace spectrum 
 {
 
+void Aria::ParallelEach(
+    std::function<void(AriaTransaction&)>   map, 
+    std::vector<AriaTransaction>&           batch
+) {
+    pool.submit_loop(
+        size_t{0}, batch.size(), 
+        [&](size_t i) { map(batch[i]); }
+    ).wait();
+}
+
+void AriaExecutor::Execute(AriaTransaction& tx, AriaTable& table) {
+    // read from the public table
+    tx.UpdateGetStorageHandler([&](
+        const evmc::address &addr,
+        const evmc::bytes32 &key
+    ) {
+        // if some write is issued previously, 
+        //  the read dependency will be barricated from journal. 
+        auto tup = std::make_tuple(addr, key);
+        if (tx.local_put.contains(tup)) {
+            return tx.local_put[tup];
+        }
+        if (tx.local_get.contains(tup)) {
+            return tx.local_get[tup];
+        }
+        auto value = evmc::bytes32{0};
+        table.Put(tup, [&](auto& entry){
+            value = entry.value;
+        });
+        tx.local_get[tup] = value;
+        return value;
+    });
+    // write locally to local storage
+    tx.UpdateSetStorageHandler([&](
+        const evmc::address &addr, 
+        const evmc::bytes32 &key,
+        const evmc::bytes32 &value
+    ) {
+        auto tup = std::make_tuple(addr, key);
+        tx.local_put[tup] = value;
+        return evmc_storage_status::EVMC_STORAGE_MODIFIED;
+    });
+    // just execute the transaction
+    tx.Execute();
+}
+
+void AriaExecutor::Reserve(AriaTransaction& tx) {
+    // write entries to reservation table
+
+    // for each written entry touched, 
+    // transactions with larger id is aborted. 
+
+}
+
+void AriaExecutor::Commit(AriaTransaction& tx) {
+    // commit changes when possible
+}
+
 } // namespace spectrum
--- lib/protocol-aria-fb.hpp
+++ lib/protocol-aria-fb.hpp
@@ -1,24 +1,68 @@
+#include <functional>
+#include <thread_pool/BS_thread_pool.hpp>
+#include <thread_pool/BS_thread_pool_utils.hpp>
+#include "table.hpp"
+#include "evm_hash.hpp"
+#include "evm_transaction.hpp"
+#include "workload.hpp"
+#include <tuple>
+#include <list>
+#include <unordered_map>
 
 namespace spectrum
 {
 
+#define K std::tuple<evmc::address, evmc::bytes32>
+#define T AriaTransaction
+
+class AriaTransaction;
+
 class Aria {
 
     private:
-    void Join(std::function<void(&AriaTransaction)>, std::vector<AriaTransaction>);
+    Workload&       workload;
+    BS::thread_pool pool;
+    void ParallelEach(
+        std::function<void(AriaTransaction&)> map, 
+        std::vector<AriaTransaction>& batch
+    );
 
     public:
-    Aria(/* args */);
-    ~Aria();
+    void Start(size_t n_threads);
 
 };
 
-class AriaTransaction {
+struct AriaTransaction: public Transaction {
+
+    std::unordered_map<K, evmc::bytes32, KeyHasher>  local_put;
+    std::unordered_map<K, evmc::bytes32, KeyHasher>  local_get;
+    AriaTransaction();
+
+};
+
+struct AriaEntry {
+    // lock transactions sorted by id increasing
+    std::list<T*>   lock_transactions   = std::list<T*>();
+    // the value of this entry
+    evmc::bytes32   value               = evmc::bytes32{0};
+};
+
+class AriaTable: public Table<K, AriaEntry, KeyHasher> {
 
 };
 
 class AriaExecutor {
 
+    public:
+    void Execute(AriaTransaction& tx, AriaTable& table);
+    void Reserve(AriaTransaction& tx);
+    void Commit(AriaTransaction& tx);
+    void AcquireLock(AriaTransaction& tx);
+    void ReleaseLock(AriaTransaction& tx);
+
 };
+
+#undef K
+#undef T
 
 } // namespace spectrum
