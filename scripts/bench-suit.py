76682fa87ce25b8befd0381a6d1ab9fb2440ecf2
Y-jiji <y.jijiji.data.science@gmail.com>
2024-03-09 12:23:22 +0800

    Feature: add a bench suit script.

--- scripts/bench-suit.py
+++ scripts/bench-suit.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     hash = subprocess.run(["git", "rev-parse", "HEAD"], **conf).stdout.decode('utf-8').strip()
     for cc in ["Aria:8:128:128:FALSE", "Sparkle:8:128", "Spectrum:8:128:16:COPYONWRITE"]: 
         print(f"#COMMIT-{hash}",  f"CONFIG-{cc}")
-        result = subprocess.run(["./build/bench", cc, "Smallbank:100000:4.0", "2s"], **conf)
+        result = subprocess.run(["./build/bench", cc, "Smallbank:100000:2.0", "2s"], **conf)
         print(result.stderr.decode('utf-8').strip())
