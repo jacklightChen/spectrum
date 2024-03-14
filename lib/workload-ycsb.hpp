f5e1cf117e193e7eb21f554f54e994f51eec2c9d
jacklightChen <chenzh@stu.ecnu.edu.cn>
2024-03-15 00:33:43 +0800

    fill ycsb bench (may have duplicate r/w)

---
+++ lib/workload-ycsb.hpp
@@ -0,0 +1,21 @@
+#include "workload.hpp"
+#include "random.hpp"
+
+namespace spectrum {
+
+class YCSB: public Workload {
+
+    private:
+    std::basic_string<uint8_t>  code;
+    EVMType                     evm_type;
+    std::unique_ptr<Random>     rng;
+    std::mutex                  mu;
+
+    public:
+    YCSB(size_t num_elements, double zipf_exponent);
+    Transaction Next() override;
+    void SetEVMType(EVMType ty) override;
+
+};
+
+};
