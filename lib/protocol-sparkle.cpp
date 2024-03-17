2e74a398e8f917d1499ea4cf24bfdb459fad4bf7
Y-jiji <y.jijiji.data.science@gmail.com>
2024-03-17 14:41:18 +0800

    Feature: add pin thread to core for sparkle.

--- lib/protocol-sparkle.cpp
+++ lib/protocol-sparkle.cpp
@@ -6,6 +6,7 @@
 #include <chrono>
 #include <glog/logging.h>
 #include <fmt/core.h>
+#include <thread-util.hpp>
 
 /*
     This is a implementation of "Sparkle: Speculative Deterministic Concurrency Control for Partially Replicated Transactional Data Stores" (Zhongmiao Li, Peter Van Roy and Paolo Romano). 
@@ -256,6 +257,7 @@ void Sparkle::Start() {
     stop_flag.store(false);
     for (size_t i = 0; i != n_dispatchers; ++i) {
         DLOG(INFO) << "start dispatcher " << i << std::endl;
+        PinRoundRobin(i);
         dispatchers.push_back(std::thread([this] {
             SparkleDispatch(*this).Run();
         }));
@@ -263,10 +265,10 @@ void Sparkle::Start() {
     for (size_t i = 0; i != n_executors; ++i) {
         DLOG(INFO) << "start executor " << i << std::endl;
         auto queue = &queue_bundle[i];
+        PinRoundRobin(i);
         executors.push_back(std::thread([this, queue] {
             SparkleExecutor(*this, *queue).Run();
         }));
-        pin_thread_to_core(executors[i]);
     }
 }
 
