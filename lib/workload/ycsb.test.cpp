813a5f016d6d9ad7ab9054bde0e0cc8be47981e0
Y-jiji <tyang119@asu.edu>
2024-04-02 14:28:39 +0800

    Feature: testing for YCSB copy on write.

---
+++ lib/workload/ycsb.test.cpp
@@ -0,0 +1,51 @@
+#include <spectrum/common/evm_transaction.hpp>
+#include <spectrum/workload/ycsb.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+
+namespace {
+
+TEST(YCSB, CopyOnWrite5R5W) {
+    auto workload = spectrum::YCSB(1000, 1.0);
+    workload.SetEVMType(spectrum::COPYONWRITE);
+    for (size_t i = 0; i < 5; ++i) {
+        for (size_t j = 0; j < 5; ++j) {
+            auto transaction = workload.Next();
+            auto checkpoints = std::vector<size_t>();
+            auto count_get = 0;
+            auto count_put = 0;
+            transaction.InstallGetStorageHandler(
+                [&](auto addr, auto key) {
+                    count_get += 1;
+                    checkpoints.push_back(transaction.MakeCheckpoint());
+                    return key;
+                }
+            );
+            transaction.InstallSetStorageHandler(
+                [&](auto addr, auto key, auto value) {
+                    count_put += 1;
+                    return evmc_storage_status::EVMC_STORAGE_ASSIGNED;
+                }
+            );
+            // execute for the first time
+            transaction.Execute();
+            ASSERT_EQ(count_get, 5);
+            ASSERT_EQ(count_put, 5);
+            // rollback and execute for the second time
+            transaction.ApplyCheckpoint(checkpoints[i]);
+            checkpoints.resize(i);
+            transaction.Execute();
+            ASSERT_EQ(count_get, 10 - i);
+            ASSERT_EQ(count_put, 10 - i);
+            // we also want to test repeated rollback, 
+            //   so let's rollback and execute again!
+            transaction.ApplyCheckpoint(checkpoints[j]);
+            checkpoints.resize(j);
+            transaction.Execute();
+            ASSERT_EQ(count_get, 15 - i - j); // when i see you again
+            ASSERT_EQ(count_put, 15 - i - j); // when i see you again ~~~
+        }
+    }
+}
+
+} // namespace
